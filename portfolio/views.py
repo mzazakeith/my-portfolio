fd339e3ae89f484befbedb725a5915f89aa3f80e
mzazakeith <mzazakeith@gmail.com>
2018-08-13 22:51:21 +0300

    new structure

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import datetime
+
+
+def get_year():
+    current_year = datetime.datetime.now().year
+    return current_year
 
 
 def index(request):
-    return render(request, 'index.html')
+    year = get_year()
+    return render(request, 'index.html', locals())
